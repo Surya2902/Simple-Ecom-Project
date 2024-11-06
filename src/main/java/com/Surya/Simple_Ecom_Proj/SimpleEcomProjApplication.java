0c78e2e14e3c077f461364a1a7bc797bf7ff8cd8
Surya02 <ssurya2910@gmail.com>
2024-11-06 11:53:12 +0530

    ecom poject

---
+++ src/main/java/com/Surya/Simple_Ecom_Proj/SimpleEcomProjApplication.java
@@ -0,0 +1,12 @@
+package com.Surya.Simple_Ecom_Proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEcomProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleEcomProjApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Surya/Simple_Ecom_Proj/SimpleEcomProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Surya.Simple_Ecom_Proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEcomProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
